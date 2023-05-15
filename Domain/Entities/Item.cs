aa4a2408aa7505e4b5c091fcb90de72ad7a2046d
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-16 00:07:56 +0700

    make properties to be optional

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -5,8 +5,8 @@ namespace Domain.Entities;
 public class Item : BaseEntity
 {
     public string Name { get; set; }
-    public string Description { get; set; }
-    public string ImagePath { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
     
     public Guid EmployeeId { get; set; }
     
