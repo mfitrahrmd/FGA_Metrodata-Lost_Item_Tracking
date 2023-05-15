31a81d7ac2b5b9c3af82294d3b83ed7a1f208ae4
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-16 01:32:30 +0700

    refactor response to success/fail

---
+++ Application/Wrappers/FailResponse.cs
@@ -0,0 +1,15 @@
+namespace Application.Wrappers;
+
+public class FailResponse<TError> : BaseResponse
+{
+    public TError Error { get; set; }
+
+    public FailResponse(string message, int code = 500) : base(false, code, message)
+    {
+    }
+
+    public FailResponse(string message, TError error, int code = 500) : base(false, code, message)
+    {
+        Error = error;
+    }
+}
