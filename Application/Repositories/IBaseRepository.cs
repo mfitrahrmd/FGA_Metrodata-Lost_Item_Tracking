3b5039a8f3958b93c7c21b816c96f97682e40cdc
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-15 21:41:59 +0700

    create project application & define repositories

---
+++ Application/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+using Domain.Common;
+
+namespace Application.Repositories;
+
+public interface IBaseRepository<TEntity>
+    where TEntity : BaseEntity
+{
+    Task InsertOneAsync(TEntity entity);
+    Task UpdateOneAsync(TEntity entity);
+    Task DeleteOneAsync(TEntity entity);
+    Task<TEntity?> FindOneByIdAsync(Guid id);
+    Task<ICollection<TEntity>> FindAllAsync();
+}
