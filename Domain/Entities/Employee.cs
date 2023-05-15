d50823e5ee409fa50744a54385f37709df5a83d0
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-15 21:40:00 +0700

    create project domain & define entities

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+using Domain.Common;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Employee : BaseEntity
+{
+    public string Nik { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public Gender Gender { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    
+    public virtual ICollection<Item>? Items { get; set; }
+}
