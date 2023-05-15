0727468ef3f751023da6dc509880383e28d68e41
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-15 22:27:39 +0700

    add service application, persistence and rest api to service container

--- RESTAPI/Program.cs
+++ RESTAPI/Program.cs
@@ -1,11 +1,13 @@
+using Persistence;
+using RESTAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceInfrastructure(builder.Configuration);
+builder.Services.AddRESTAPIServices(builder.Configuration);
 
 var app = builder.Build();
 
