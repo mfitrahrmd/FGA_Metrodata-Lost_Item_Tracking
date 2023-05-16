7422d6f856586f14355cd74eebfcf210f3c3e61c
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-16 20:23:18 +0700

    migration create role&accountroles entities

---
+++ Persistence/Migrations/20230516132004_create_role&accountroles_entity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class create_roleaccountroles_entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "account_roles",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    account_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    role_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_account_roles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_account_roles_accounts_account_temp_id",
+                        column: x => x.account_id,
+                        principalTable: "accounts",
+                        principalColumn: "employee_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_account_roles_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_account_roles_account_id",
+                table: "account_roles",
+                column: "account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_account_roles_role_id",
+                table: "account_roles",
+                column: "role_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "account_roles");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
