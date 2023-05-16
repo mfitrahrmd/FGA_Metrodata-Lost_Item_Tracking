393149a9f0134485a78aa0f1c568b961fb213f9e
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-16 15:12:00 +0700

    define rest api configure service

--- RESTAPI/ConfigureServices.cs
+++ RESTAPI/ConfigureServices.cs
@@ -1,12 +1,25 @@
+using System.Text.Json.Serialization;
+
 namespace RESTAPI;
 
 public static class ConfigureServices
 {
     public static void AddRESTAPIServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddControllers();
+        services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.DefaultIgnoreCondition =
+                JsonIgnoreCondition.WhenWritingNull;
+            options.JsonSerializerOptions.ReferenceHandler =
+                ReferenceHandler.IgnoreCycles;
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        services.AddRouting(options =>
+        {
+            options.LowercaseUrls = true;
+        });
     }
 }
