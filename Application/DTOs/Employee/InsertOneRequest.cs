c7eb0c1a689d9cc1afccbb5a2958d172dfc630b9
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-16 14:32:29 +0700

    define insert one employee request

---
+++ Application/DTOs/Employee/InsertOneRequest.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+
+namespace Application.DTOs.Employee;
+
+public class InsertOneRequest
+{
+    public string Nik { get; set; }
+    public string FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    [EnumDataType(typeof(Gender))] public Gender Gender { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
