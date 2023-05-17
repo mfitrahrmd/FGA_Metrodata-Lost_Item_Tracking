573cab8a0c61c481c44b906bf2eb3990acb15659
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-17 11:37:24 +0700

    add mapper DTOs

--- Application/Mappings/Profiles.cs
+++ Application/Mappings/Profiles.cs
@@ -1,6 +1,12 @@
+using Application.DTOs.Account;
+using Application.DTOs.Action;
+using Application.DTOs.Department;
 using Application.DTOs.Employee;
+using Application.DTOs.Item;
+using Application.DTOs.Role;
 using AutoMapper;
 using Domain.Entities;
+using Action = Domain.Entities.Action;
 
 namespace Application.Mappings;
 
@@ -10,5 +16,10 @@ public class Profiles : Profile
     {
         CreateMap<InsertOneEmployeeRequest, Employee>();
         CreateMap<Employee, EmployeeDTO>();
+        CreateMap<Account, AccountDTO>();
+        CreateMap<Action, ActionDTO>();
+        CreateMap<Department, DepartmentDTO>();
+        CreateMap<Item, ItemDTO>();
+        CreateMap<Role, RoleDTO>();
     }
 }
