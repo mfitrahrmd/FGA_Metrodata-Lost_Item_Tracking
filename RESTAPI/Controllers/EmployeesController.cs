2d656d3da5df8e0c11aa6b0dc8681bea0d730d18
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-17 11:39:11 +0700

    implement employees controller

---
+++ RESTAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+using Application.DTOs.Employee;
+using Application.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTAPI.Controllers;
+
+[ApiController]
+public class EmployeesController : BaseController<Employee, IEmployeeRepository, EmployeeDTO, InsertOneEmployeeRequest>
+{
+    public EmployeesController(IEmployeeRepository repository, IMapper mapper) : base(repository, mapper)
+    {
+    }
+}
