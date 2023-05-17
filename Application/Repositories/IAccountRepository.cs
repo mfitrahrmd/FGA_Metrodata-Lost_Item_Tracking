1fb90e562c8306fbf7748fee2208e0418ca71154
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-17 21:41:15 +0700

    add method GetRolesOfAnAccountByEmployeeId

--- Application/Repositories/IAccountRepository.cs
+++ Application/Repositories/IAccountRepository.cs
@@ -1,7 +1,9 @@
+using Application.DAOs.Account;
 using Domain.Entities;
 
 namespace Application.Repositories;
 
 public interface IAccountRepository : IBaseRepository<Account>
 {
+    Task<RolesOfAnAccount> GetRolesOfAnAccountByEmployeeId(Guid id);
 }
--- Persistence/Repositories/AccountRepository.cs
+++ Persistence/Repositories/AccountRepository.cs
@@ -1,5 +1,7 @@
+using Application.DAOs.Account;
 using Application.Repositories;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Persistence.Context;
 
 namespace Persistence.Repositories;
@@ -8,5 +10,10 @@ public class AccountRepository : BaseRepository<Account, ApplicationDbContext>, 
 {
     public AccountRepository(ApplicationDbContext context) : base(context)
     {
+    }
+
+    public async Task<RolesOfAnAccount> GetRolesOfAnAccountByEmployeeId(Guid id)
+    {
+        return await _set.Where(a => a.EmployeeId.Equals(id)).Select(a => new RolesOfAnAccount{Roles = a.AccountRoles.Select(ar => ar.Role.Name).ToList()}).FirstOrDefaultAsync();
     }
 }
