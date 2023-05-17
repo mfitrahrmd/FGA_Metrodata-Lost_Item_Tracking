5fe6aee33c10b5f9e089d2217ab201aca0e2add0
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-17 11:37:00 +0700

    define action, item and role DTOs

---
+++ Application/DTOs/Action/ActionDTO.cs
@@ -0,0 +1,7 @@
+namespace Application.DTOs.Action;
+
+public class ActionDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ Application/DTOs/Role/RoleDTO.cs
@@ -0,0 +1,7 @@
+namespace Application.DTOs.Role;
+
+public class RoleDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
