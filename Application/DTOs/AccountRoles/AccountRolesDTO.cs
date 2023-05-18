3ed8adbb32184c4eaeb305af2d484626474b3169
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-19 03:32:06 +0700

    define accountroles DTO

---
+++ Application/DTOs/AccountRoles/AccountRolesDTO.cs
@@ -0,0 +1,8 @@
+namespace Application.DTOs.AccountRoles;
+
+public class AccountRolesDTO
+{
+    public Guid Id { get; set; }
+    public Guid AccountId { get; set; }
+    public Guid RoleId { get; set; }
+}
