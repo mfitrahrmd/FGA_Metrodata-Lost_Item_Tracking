c552a526f6a3f9d025b1ae12d47928f974473fbd
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-18 10:37:49 +0700

    define register req/res DTO

---
+++ Application/DTOs/Account/RegisterRequest.cs
@@ -0,0 +1,16 @@
+using Domain.Enums;
+
+namespace Application.DTOs.Account;
+
+public class RegisterRequest
+{
+    public string Nik { get; set; }
+    public string FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public Gender Gender { get; set; }
+    public string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public Guid DepartmentId { get; set; }
+    public string Password { get; set; }
+}
---
+++ Application/DTOs/Account/RegisterResponse.cs
@@ -0,0 +1,6 @@
+namespace Application.DTOs.Account;
+
+public class RegisterResponse
+{
+    public string Nik { get; set; }
+}
