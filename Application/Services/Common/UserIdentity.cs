11d758092aa3639267975784bddbbf3a798bb192
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-19 23:58:22 +0700

    define UserIdentity model

--- Application/Services/Common/UserIdentity.cs
+++ Application/Services/Common/UserIdentity.cs
@@ -2,9 +2,9 @@ namespace Application.Services.Common;
 
 public struct UserIdentity
 {
-    public Guid Id { get; }
-    public string Nik { get; }
-    public IList<string> Roles { get; }
+    public Guid Id { get; set; }
+    public string Nik { get; set; }
+    public IList<string> Roles { get; set; }
 
     public UserIdentity(Guid id, string nik, IList<string> roles)
     {
