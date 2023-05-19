9a63fa2a9d5d929dc36838e2a144e2ac9417687c
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-20 00:20:59 +0700

    remove property is_approved

--- Domain/Entities/ItemActions.cs
+++ Domain/Entities/ItemActions.cs
@@ -9,8 +9,6 @@ namespace Domain.Entities;
 public class ItemActions : BaseEntity
 {
     public DateTime Time { get; set; }
-    public bool IsApproved { get; set; } = false;
-    
     public Guid ItemId { get; set; }
     public Guid ActionId { get; set; }
     public Guid EmployeeId { get; set; }
