3b0bdfdcc62e96262e2cd5ec09b4b9476b4ce2c2
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-20 00:20:37 +0700

    migration create table status

---
+++ Persistence/Migrations/20230519171744_create_table_status.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class create_table_status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "status",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    message = table.Column<string>(type: "text", nullable: false),
+                    item_actions_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_status", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_status_item_actions_item_actions_id",
+                        column: x => x.item_actions_id,
+                        principalTable: "item_actions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_status_item_actions_id",
+                table: "status",
+                column: "item_actions_id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "status");
+        }
+    }
+}
