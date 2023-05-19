f243f3322d70533057661fd1b922d84d788650b9
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-20 01:26:43 +0700

    migration edit_itemactions_only_have_exacly_one_status

--- Persistence/Migrations/20230519182456_edit_itemactions_only_have_exacly_one_status.Designer.cs
+++ Persistence/Migrations/20230519182456_edit_itemactions_only_have_exacly_one_status.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Context;
 
@@ -11,9 +12,10 @@ using Persistence.Context;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230519182456_edit_itemactions_only_have_exacly_one_status")]
+    partial class edit_itemactions_only_have_exacly_one_status
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20230519182456_edit_itemactions_only_have_exacly_one_status.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class edit_itemactions_only_have_exacly_one_status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_status",
+                table: "status");
+
+            migrationBuilder.DropIndex(
+                name: "ix_status_item_actions_id",
+                table: "status");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "status");
+
+            migrationBuilder.DropColumn(
+                name: "is_approved",
+                table: "item_actions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_status",
+                table: "status",
+                column: "item_actions_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_status",
+                table: "status");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                table: "status",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_approved",
+                table: "item_actions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_status",
+                table: "status",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_status_item_actions_id",
+                table: "status",
+                column: "item_actions_id",
+                unique: true);
+        }
+    }
+}
