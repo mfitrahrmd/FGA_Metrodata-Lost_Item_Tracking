00c1231fe4b8f7d3bff62058baafd0215edac596
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-21 16:12:08 +0700

    refactor RequestFoundItem model

--- Application/DAOs/Item/PendingFoundItemRequestClaim.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Entities;
-
-namespace Application.DAOs.Item;
-
-public class PendingFoundItemRequestClaim : Domain.Entities.Item
-{
-    public DateTime RequestAt { get; set; }
-    public Employee RequestBy { get; set; }
-}
