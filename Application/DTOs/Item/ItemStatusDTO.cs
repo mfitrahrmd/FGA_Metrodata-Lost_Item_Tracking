23d9b50cda53a061aa169eb873bdf78b1971478c
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-28 17:18:05 +0700

    define status dto

---
+++ Application/DTOs/Item/ItemStatusDTO.cs
@@ -0,0 +1,6 @@
+namespace Application.DTOs.Item;
+
+public class ItemStatusDTO
+{
+    public string Name { get; set; }
+}
---
+++ Application/DTOs/Status/StatusDTO.cs
@@ -0,0 +1,8 @@
+namespace Application.DTOs.Status;
+
+public class StatusDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Message { get; set; }
+}
