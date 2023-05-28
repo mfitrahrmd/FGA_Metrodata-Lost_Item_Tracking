f583331cf44aeddff16d6de56f55fdc0cd2df656
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-28 17:25:45 +0700

    define & implement item actions repository

--- Application/Repositories/IItemActionsRepository.cs
+++ Application/Repositories/IItemActionsRepository.cs
@@ -1,3 +1,6 @@
+using Application.DAOs.Item;
+using Application.DTOs.Item;
+using Application.Services.Common;
 using Domain.Entities;
 
 namespace Application.Repositories;
@@ -5,4 +8,5 @@ namespace Application.Repositories;
 public interface IItemActionsRepository : IBaseRepository<ItemActions>
 {
     Task<ItemActions?> FindOneByItemIdAndActionName(Guid itemId, string actionName);
+    Task<ICollection<ItemActions>> FindAllByActionNameAndEmployeeId(ActionType actionType, Guid employeeId, ActionRequestQuery query);
 }
--- Persistence/Repositories/ItemActionsRepository.cs
+++ Persistence/Repositories/ItemActionsRepository.cs
@@ -1,4 +1,6 @@
+using Application.DTOs.Item;
 using Application.Repositories;
+using Application.Services.Common;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Context;
@@ -14,5 +16,17 @@ public class ItemActionsRepository : BaseRepository<ItemActions, ApplicationDbCo
     public async Task<ItemActions?> FindOneByItemIdAndActionName(Guid itemId, string actionName)
     {
         return await _set.FirstOrDefaultAsync(ia => ia.ItemId.Equals(itemId) && ia.Action.Name.Equals(actionName));
+    }
+
+    public async Task<ICollection<ItemActions>> FindAllByActionNameAndEmployeeId(ActionType actionType, Guid employeeId, ActionRequestQuery query)
+    {
+        var result = _set
+            .Include(ia => ia.Item)
+            .Include(ia => ia.Action)
+            .Include(ia => ia.Employee)
+            .Include(ia => ia.Status)
+            .Where(ia => ia.EmployeeId.Equals(employeeId) && ia.Action.Name.Equals(actionType.ToString()) && (query.Status == null || ia.Status.Name.Equals(query.Status)));
+
+        return await result.ToListAsync();
     }
 }
