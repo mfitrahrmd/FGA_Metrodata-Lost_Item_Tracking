860bdee25d0166438795523d614853e0e993a967
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-28 17:30:41 +0700

    register DTOs to mapper

--- Application/Mappings/Profiles.cs
+++ Application/Mappings/Profiles.cs
@@ -9,6 +9,8 @@ using Application.DTOs.Employee;
 using Application.DTOs.Item;
 using Application.DTOs.ItemActions;
 using Application.DTOs.Role;
+using Application.DTOs.Status;
+using Application.DTOs.User;
 using AutoMapper;
 using Domain.Entities;
 using Action = Domain.Entities.Action;
@@ -21,7 +23,6 @@ public class Profiles : Profile
 {
     public Profiles()
     {
-        CreateMap<InsertOneEmployeeRequest, Employee>();
         CreateMap<Employee, EmployeeDTO>();
         CreateMap<Account, AccountDTO>();
         CreateMap<Action, ActionDTO>();
@@ -34,5 +35,23 @@ public class Profiles : Profile
         CreateMap<AccountRoles, AccountRolesDTO>();
         CreateMap<ApprovedFoundItem, FoundItemDTO>();
         CreateMap<RequestFoundItem, FoundItemDTO>();
+        CreateMap<Status, StatusDTO>().ForMember(sd => sd.Id, expression =>
+        {
+            expression.MapFrom(s => s.ItemActionsId);
+        });
+        CreateMap<ItemActions, MyRequestFoundResponse>();
+        CreateMap<ItemActions, MyRequestClaimResponse>();
+        CreateMap<Application.DTOs.Item.InsertItemRequest, Item>();
+        CreateMap<Application.DTOs.Item.UpdateItemRequest, Item>();
+        CreateMap<Application.DTOs.Employee.InsertEmployeeRequest, Employee>();
+        CreateMap<Application.DTOs.Employee.UpdateEmployeeRequest, Employee>();
+        CreateMap<Application.DTOs.Account.InsertAccountRequest, Account>();
+        CreateMap<Application.DTOs.Account.UpdateAccountRequest, Account>();
+        CreateMap<Application.DTOs.Department.InsertDepartmentRequest, Department>();
+        CreateMap<Application.DTOs.Department.UpdateDepartmentRequest, Department>();
+        CreateMap<Application.DTOs.AccountRoles.InsertAccountRolesRequest, AccountRoles>();
+        CreateMap<Application.DTOs.AccountRoles.UpdateAccountRolesRequest, AccountRoles>();
+        CreateMap<Application.DTOs.Role.InsertRoleRequest, Role>();
+        CreateMap<Application.DTOs.Role.UpdateRoleRequest, Role>();
     }
 }
