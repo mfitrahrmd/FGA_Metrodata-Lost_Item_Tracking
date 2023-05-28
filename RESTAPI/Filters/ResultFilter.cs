11705cc746bc3180605bf4780d0ea7e7e754e160
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-28 17:26:23 +0700

    unimplement result filter

--- RESTAPI/Filters/ResultFilter.cs
+++ RESTAPI/Filters/ResultFilter.cs
@@ -13,22 +13,4 @@ public class ResultFilter : ResultFilterAttribute
     {
         _configuration = configuration;
     }
-    
-    public override void OnResultExecuting(ResultExecutingContext context)
-    {
-        var ok = context.Result as ObjectResult;
-
-        if (ok != null)
-        {
-            var data = ok.Value as SuccessResponse<ItemDTO>;
-
-            if (data != null)
-            {
-                var baseUrl =
-                    $"{context.HttpContext.Request.Scheme}://{context.HttpContext.Request.Host}{context.HttpContext.Request.PathBase}";
-                var photosPath = _configuration["Application:ItemPhotosPath"].Substring(1);
-                data.Data.ImagePath = $"{baseUrl}{photosPath}{data.Data.ImagePath}";
-            }
-        }
-    }
 }
