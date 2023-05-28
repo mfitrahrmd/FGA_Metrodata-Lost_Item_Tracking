992045a830089328711ea3f8d1869984f909ae6e
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-28 17:30:04 +0700

    implement insert&update controller

--- RESTAPI/Controllers/BaseController.cs
+++ RESTAPI/Controllers/BaseController.cs
@@ -13,9 +13,11 @@ namespace RESTAPI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public abstract class BaseController<TEntity, TRepository, TDTO> : ControllerBase
+public abstract class BaseController<TEntity, TRepository, TInsert, TUpdate, TDTO> : ControllerBase
     where TEntity : BaseEntity
     where TRepository : IBaseRepository<TEntity>
+    where TInsert : class
+    where TUpdate : class
     where TDTO : class
 {
     protected readonly TRepository Repository;
@@ -30,21 +32,36 @@ public abstract class BaseController<TEntity, TRepository, TDTO> : ControllerBas
     [HttpGet]
     public async Task<ActionResult<SuccessResponse<ICollection<TDTO>>>> FindAllAsync()
     {
-        var entities = await Repository.FindAllAsync();
+        try
+        {
 
-        return Ok(new SuccessResponse<ICollection<TDTO>>(null, _mapper.Map<ICollection<TDTO>>(entities)));
+            var entities = await Repository.FindAllAsync();
+
+            return Ok(new SuccessResponse<ICollection<TDTO>>(null, _mapper.Map<ICollection<TDTO>>(entities)));
+        }
+        catch (Exception e)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, new FailResponse<string>("Unexpected server error."));
+        }
     }
 
     [HttpGet]
     [Route("{id}")]
     public async Task<ActionResult<SuccessResponse<TDTO>>> FindOneByIdAsync([FromRoute] Guid id)
     {
-        var entity = await Repository.FindOneByIdAsync(id);
+        try
+        {
+            var entity = await Repository.FindOneByIdAsync(id);
 
-        if (entity is null)
-            return NotFound(new FailResponse<string>($"{nameof(TEntity)} was not found with given id."));
+            if (entity is null)
+                return NotFound(new FailResponse<string>($"{typeof(TEntity).Name} was not found with given id."));
 
-        return Ok(new SuccessResponse<TDTO>(null, _mapper.Map<TDTO>(entity)));
+            return Ok(new SuccessResponse<TDTO>(null, _mapper.Map<TDTO>(entity)));
+        }
+        catch (Exception e)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, new FailResponse<string>("Unexpected server error."));
+        }
     }
 
     [HttpDelete]
@@ -55,9 +72,40 @@ public abstract class BaseController<TEntity, TRepository, TDTO> : ControllerBas
         {
             await Repository.DeleteOneByIdAsync(id);
 
-            return Ok(new SuccessResponse($"{nameof(TEntity)} successfully deleted."));
+            return Ok(new SuccessResponse($"{typeof(TEntity).Name} successfully deleted."));
         }
         catch (Exception e)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, new FailResponse<string>("Unexpected server error."));
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<SuccessResponse>> InsertOneAsync([FromBody] TInsert data)
+    {
+        try
+        {
+            await Repository.InsertOneAsync(_mapper.Map<TEntity>(data));
+
+            return Ok(new SuccessResponse($"{typeof(TEntity).Name} successfully inserted."));
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, new FailResponse<string>("Unexpected server error."));
+        }
+    }
+    
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<ActionResult<SuccessResponse>> UpdateOneAsync([FromRoute] Guid id, [FromBody] TUpdate data)
+    {
+        try
+        {
+            await Repository.UpdateOneByIdAsync(id, _mapper.Map<TEntity>(data));
+
+            return Ok(new SuccessResponse($"{typeof(TEntity).Name} successfully updated."));
+        }
+        catch (Exception)
         {
             return StatusCode((int)HttpStatusCode.InternalServerError, new FailResponse<string>("Unexpected server error."));
         }
